ba9172cfdd2322b28a627ad4e9f642871f2fcf94
alnig441 <alnig441@gmail.com>
2017-08-24 10:34:55 -0400

    bootstrap 3/4 added

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -68,12 +68,6 @@ module.exports = {
             {
                 // your Angular Async Route paths relative to this root directory
             }
-        ),
-        new webpack.ProvidePlugin({ // inject ES5 modules as global vars
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Tether: 'tether'
-        })
+        )
     ]
 };
