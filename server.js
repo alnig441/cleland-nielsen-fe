dda64779093433c919deadc58cf670228bcd3fec
alnig441 <alnig441@gmail.com>
2017-09-18 17:01:30 -0400

    minor change

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const config = require('./config/webpack.prod.js');
 const configDev = require('./config/webpack.dev.js');
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const url = isDeveloping ? "localhost" : "0.0.0.0";
+const hostName = isDeveloping ? "localhost" : "0.0.0.0";
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
 
 console.log('is prod? ', isDeveloping !== 'production', process.env.NODE_ENV);
 
 if (isDeveloping) {
-    console.log('dev mode');
+    console.log('running dev mode (express)');
     const compiler = webpack(config);
     const middleware = webpackMiddleware(compiler, {
         publicPath: config.output.publicPath,
@@ -38,16 +38,16 @@ if (isDeveloping) {
         res.end();
     });
 } else {
-    console.log('prod mode');
+    console.log('running prod mode (express)');
     app.use(express.static(__dirname + '/dist'));
     app.get('*', function response(req, res) {
         res.sendFile(path.join(__dirname, 'dist/index.html'));
     });
 }
 
-app.listen(port, url, function onStart(err) {
+app.listen(port, hostName, function onStart(err) {
     if (err) {
         console.log(err);
     }
-    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+    console.info('==> 🌎 Listening on port %s. Open up http://'+hostName+':%s/ in your browser.', port, port);
 });
