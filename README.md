87de321bdef14c644fb6d3eceffbbf9a5bc4c30f
alnig441 <alnig441@gmail.com>
2017-09-18 16:51:28 -0400

    script added to run dev or prod server with express

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ A basic Angular 4 seed project utilizing the following technologies:
 * `npm test` - run the project unit tests (*.spec.ts files)
 * `npm run lint` - run the project linting (will be run every time `npm test` is run)
 * `npm run build` - generate a production build for the project, which will be inserted into dist/
+* `npm run express` - run development or production server through express
---
+++ server.js
@@ -0,0 +1,53 @@
+/* configuration of express server to run production environment */
+
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./config/webpack.prod.js');
+const configDev = require('./config/webpack.dev.js');
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const url = isDeveloping ? "localhost" : "0.0.0.0";
+const port = isDeveloping ? 3000 : process.env.PORT;
+const app = express();
+
+console.log('is prod? ', isDeveloping !== 'production', process.env.NODE_ENV);
+
+if (isDeveloping) {
+    console.log('dev mode');
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        contentBase: 'src',
+        stats: {
+            colors: true,
+            hash: false,
+            timings: true,
+            chunks: false,
+            chunkModules: false,
+            modules: false
+        }
+    });
+
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('*', function response(req, res) {
+        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+        res.end();
+    });
+} else {
+    console.log('prod mode');
+    app.use(express.static(__dirname + '/dist'));
+    app.get('*', function response(req, res) {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+}
+
+app.listen(port, url, function onStart(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
