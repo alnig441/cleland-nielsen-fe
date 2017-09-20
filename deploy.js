8dcad3f8a79194d3c5b9855018363e952f4b1d8d
alnig441 <alnig441@gmail.com>
2017-09-20 13:37:28 -0400

    deploy script amended;

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     var child_process = require('child_process');
-    child_process.exec("rimraf dist && webpack --config config/webpack.prod.js --progress --profile --bail", function (error, stdout, stderr) {
+    child_process.exec("webpack -p --config config/webpack.prod.js", function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
