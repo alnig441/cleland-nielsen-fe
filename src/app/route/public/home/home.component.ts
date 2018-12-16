f9bef1b9d284f7bf080147d0ffe44fec6322ba28
Allan Nielsen <alnig441@gmail.com>
2018-12-16 07:14:18 -0500

    clean-up

--- src/app/route/public/home/home.component.ts
+++ src/app/route/public/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-const SiteCopy = require("../../../../../api/site_copy.json");
 
 @Component({
     selector: "app-home",
@@ -11,13 +10,10 @@ export class HomeComponent implements OnInit {
 
     constructor(private http: HttpClient) {}
 
-    private home: string[] = SiteCopy.Home;
-
     public ngOnInit(): void {
         // console.log('home component initialised\nLocalStorage: ', localStorage);
     }
 }
 
 interface ItemsResponse {
-    Home: string[];
 }
