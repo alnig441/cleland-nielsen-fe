0dc215b6bd430e4d2b7c8869a9655a2d4f2ea5a3
Allan Nielsen <alnig441@gmail.com>
2018-12-19 15:39:54 -0500

    hostlisteners added to app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation, HostListener } from "@angular/core";
+import { AuthenticationService } from "./services/authentication.service";
 
 @Component({
     selector: "app-root",
@@ -8,11 +9,43 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 })
 export class AppComponent implements OnInit {
 
-    constructor() {}
+    constructor(private user : AuthenticationService ) {}
 
     public ngOnInit(): void {
 
         console.log('app comp init');
 
+    }
+
+    @HostListener('window:mousemove', ['$event'])
+    moveEvent(event: any) {
+
+        if (this.user.isLoggedIn) {
+            console.log('mouse moved detected: ', event);
+        }
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    scrollEvent(event: any) {
+
+        if (this.user.isLoggedIn) {
+            console.log('scroll detected: ', event);
+        }
+    }
+
+    @HostListener('window:keypress', ['$event'])
+    pressEvent(event: any) {
+
+        if (this.user.isLoggedIn) {
+            console.log('key press detected: ', event);
+        }
+    }
+
+    @HostListener('window:click', ['$event'])
+    clickEvent(event: any) {
+
+        if (this.user.isLoggedIn) {
+            console.log('click detected: ', event);
+        }
     }
 }
