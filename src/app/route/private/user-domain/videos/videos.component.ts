fa19c27715eb1aa6c7fa2b1e212a55053e6cf82d
Allan Nielsen <alnig441@gmail.com>
2018-12-19 15:38:45 -0500

    logging removed from video component

--- src/app/route/private/user-domain/videos/videos.component.ts
+++ src/app/route/private/user-domain/videos/videos.component.ts
@@ -14,7 +14,6 @@ export class VideosComponent implements OnInit {
     constructor(private formManager: ServiceModelManagerService, private activeUser: AuthenticationService, private activatedRoute: ActivatedRoute){}
 
     ngOnInit(): void {
-        console.log('events comp init', this.activeUser.isPermitted);
         this.formManager.setService(this.activatedRoute.snapshot.url[0].path);
     }
 }
