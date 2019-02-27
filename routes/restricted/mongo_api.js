2574f8a15b1b2aaab2e7d48e432f4f10305d73b9
Allan Nielsen <alnig441@gmail.com>
2019-02-27 16:39:13 -0500

    semantic changes + res.send

--- routes/restricted/mongo_api.js
+++ routes/restricted/mongo_api.js
@@ -1,18 +1,19 @@
 const express = require('express'),
     router = express.Router();
 
-const http = require('request');
+const api = require('request');
 const basic_auth = `Basic  ${new Buffer(process.env.MONGO_API_USER + ':' + process.env.MONGO_API_PW).toString('base64')}`
 
 router.get('/Search/Photos', (req, res, next) => {
 
-  request.get({
+  api.get({
     url: `${process.env.MONGO_API_BASE_URL}Search/Photos`,
     headers: {
       'Authorization': basic_auth
     }
-  }, (err, res, body) => {
+  }, (err, result, body) => {
     console.log('any response: ', body);
+    res.send(body);
   })
 })
 
