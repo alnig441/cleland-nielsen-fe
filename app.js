0f16c85f2d40681afe50b85a59168e2363323145
Allan Nielsen <alnig441@gmail.com>
2019-04-02 09:35:14 -0400

    schedule and associated app_modules files removed

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const logger = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const cron = require('node-cron');
-const fs = require('fs');
-
-const jobHandler = require('./app_modules/jobHandler');
-var photoAppJob;
 
 require('./routes/authenticate/passport');
 
@@ -45,71 +40,5 @@ app.use('/api', passport.authenticate('jwt', {session: false}), mongo_api);
 app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
-if (process.env.SCHEDULE) {
-  cron.schedule(process.env.SCHEDULE, () => {
-
-      const logFile = '.photoapp.log';
-
-      fs.writeFile(logFile, `LOG OPENED: ${new Date()}`, (err) => {
-          return null;
-      });
-
-      photoAppJob = new jobHandler('/photoapptemp/');
-      console.log('cron job started at: ', new Date())
-
-      photoAppJob.on('dto_done', listenDtoDone);
-      photoAppJob.on('conversion_done', listenConversionDone);
-      photoAppJob.on('db_load_done', listenDbLoadDone);
-      photoAppJob.on('empty', listenEmpty);
-      photoAppJob.on('error', listenError);
-
-      photoAppJob.generateDto();
-
-      function listenDtoDone(DTO) {
-          console.log(`job generate DTO complete - DTO generated for ${DTO.length} new images`)
-          if(DTO && DTO.length > 0){
-              photoAppJob.loadImages(DTO);
-          } else {
-              console.log(`cron schedule terminated`);
-              photoAppJob.removeAllListeners();
-              let logMessage = `\n${new Date()}: INFO: CRON Schedule Terminated: Empty DTO`;
-              writeToLog(logMessage);
-          }
-      }
-
-      function listenDbLoadDone(loadedImages) {
-          console.log(`job load db complete - ${loadedImages.length} new images loaded into images table`);
-          photoAppJob.convertFilesToPng();
-      }
-
-      function listenConversionDone(processedFiles) {
-          console.log(`job file conversion / cron schedule completed - ${processedFiles.length} files converted and moved`);
-          photoAppJob.removeAllListeners();
-          let logMessage = `\n${new Date()}: SUCCESS: CRON Schedule Completed: ${processedFiles.length} files converted and moved`;
-          writeToLog(logMessage);
-      }
-
-      function listenEmpty(res) {
-          console.log(`cron schedule terminated - ${res}`);
-          photoAppJob.removeAllListeners();
-      }
-
-      function listenError(err) {
-          console.log('cron schedule terminated - error emitted: ', err);
-          photoAppJob.removeAllListeners();
-          let logMessage = `\n${new Date()}: ERROR: CRON Schedule Terminated: ${err.code} - ${err.detail}`;
-          writeToLog(logMessage);
-      }
-
-      function writeToLog(message) {
-          fs.appendFile(logFile, message, (err) => {
-              return null;
-          })
-      }
-
-
-  })
-}
 
 module.exports = app;
