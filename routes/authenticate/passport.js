949cbb5e0072fe6b890d83cdcdbcca3c0d5261b4
Allan Nielsen <alnig441@gmail.com>
2019-04-03 11:36:59 -0400

    connection pool for authentication added

--- routes/authenticate/passport.js
+++ routes/authenticate/passport.js
@@ -4,7 +4,8 @@ const passport = require('passport'),
     ExtractJWT = require('passport-jwt').ExtractJwt;
 
 const jwtSecret = process.env.JWT_SECRET || 'some_secret_word';
-const { Client } = require('pg');
+const { Pool } = require('pg');
+
 const connectionString = process.env.MYDB || 'postgresql://allannielsen:1109721405@localhost:5432/jacn2014_ng4';
 
 const jwt = require('jsonwebtoken');
@@ -12,62 +13,58 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt'),
     saltRounds = 10;
 
+
+const pool = new Pool({
+  connectionString: connectionString
+})
+
 passport.use(new localStrategy({
     usernameField: 'username',
     passwordField: 'password'
 }, (username, password, cb) => {
-
-
-    const client = new Client({
-        connectionString: connectionString,
-    })
-    client.connect();
-
-    return client.query(`SELECT * FROM users as a INNER JOIN accounts as b ON a.account_type::uuid = b.account_id where a.user_name='${username}'`)
+  pool.connect()
+    .then(client => {
+      return client.query(`SELECT * FROM users as a INNER JOIN accounts as b ON a.account_type::uuid = b.account_id where a.user_name='${username}'`)
         .then(result => {
-            // console.log('result from user query in passport: ', result.rows)
-            let user = result.rows[0]
+          client.release();
+          let user = result.rows[0];
+          if (user) {
             bcrypt.compare(password, user.password)
-                .then(match => {
-                    if(match) {
-                        return cb(null, user, {message: 'login success!'})
-                    }
-                    else {
-                        return cb({message: 'invalid password'})
-                    }
-                    client.end();
-                })
+              .then(match => {
+                if(match) {
+                  return cb(null, user, { message: 'login success!' })
+                }
+                else {
+                  return cb({ message: 'invalid password' })
+                }
+              })
+          } else {
+            return cb({ message: 'invalid username' })
+          }
         })
         .catch(err => {
-            return cb({message: 'user name does not exist'})
-            client.end();
+          client.release();
+          return cb({ message: err })
         })
-
+    })
 }));
 
 passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: jwtSecret
-    }, (jwtPayload, cb) => {
-
-        // console.log('in jwt strategy: ', jwtPayload)
-
-        const client = new Client({
-            connectionString: connectionString
-        });
-
-        client.connect();
-
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: jwtSecret
+  }, (jwtPayload, cb) => {
+    pool.connect()
+      .then(client => {
         return client.query(`SELECT * FROM users where user_id='${jwtPayload.sub}'`)
-            .then(result => {
-                let user = result.rows;
-                return cb(null, user);
-                client.end();
-            })
-            .catch(err => {
-                return cb (err);
-                client.end();
-            })
-
-    }
+          .then(result => {
+            client.release();
+            let user = result.rows;
+            return cb(null, user);
+          })
+          .catch(err => {
+            client.release();
+            return cb (err);
+          })
+      })
+  }
 ));
