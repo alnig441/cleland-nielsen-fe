11ef0e4eba10448f1945d5f2a13e1e2e5fc20dae
Allan Nielsen <alnig441@gmail.com>
2019-04-05 16:36:06 -0400

    image-editior directive added to user-domain module

--- src/app/route/private/user-domain/user-domain.module.ts
+++ src/app/route/private/user-domain/user-domain.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { UserDomainRoutingModule } from "./user-domain-routing.module";
-import { VideosComponent } from "./videos/videos.component";
-import { AuthenticationGuardService } from "../../../services/authentication-guard.service";
-import { ImagesComponent } from "./images/images.component";
-import { UserDomainComponent } from "./user-domain.component";
 import { MessageBarModule } from "../../../modules/message-bar/message-bar.module";
 import { SidebarCtaModule } from "../../../modules/sidebar-cta/sidebar-cta.module";
 import { FormSubmissionModule } from "../../../modules/form-submission/form-submission.module";
+
+import { VideosComponent } from "./videos/videos.component";
+import { ImagesComponent } from "./images/images.component";
+import { UserDomainComponent } from "./user-domain.component";
+
 import { MonthTransform } from "../../../pipes/month.transform";
-import { InfoboxDirective } from "../../../directives/infobox.directive";
 import { KeyTransform } from "../../../pipes/key.transform";
 import { ValueTransform } from "../../../pipes/value.transform";
-import { ModalImageDirective } from "../../../directives/modalImage.directive";
 import { ExtTransform } from "../../../pipes/ext.transform";
 import { BtnTransform } from "../../../pipes/btn.transform";
 import { DayTransform } from "../../../pipes/day.transform";
+
+import { InfoboxDirective } from "../../../directives/infobox.directive";
+import { ModalImageDirective } from "../../../directives/modalImage.directive";
+import { ImageEditorDirective } from "../../../directives/image-editor.directive";
+
+import { AuthenticationGuardService } from "../../../services/authentication-guard.service";
 
 @NgModule({
     imports: [
@@ -36,7 +41,8 @@ import { DayTransform } from "../../../pipes/day.transform";
         BtnTransform,
         DayTransform,
         ModalImageDirective,
-        InfoboxDirective
+        InfoboxDirective,
+        ImageEditorDirective
     ],
     exports: [ MonthTransform, KeyTransform, ValueTransform, ExtTransform, BtnTransform, DayTransform ],
     providers: [ AuthenticationGuardService ]
