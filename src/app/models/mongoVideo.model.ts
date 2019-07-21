54014e95ca2e0819430b64883f2c3519269766d1
Allan Nielsen <alnig441@gmail.com>
2019-07-21 11:00:16 -0400

    video model added

---
+++ src/app/models/mongoVideo.model.ts
@@ -0,0 +1,18 @@
+export class MongoVideoModel {
+    constructor(
+        public created?: string,
+        public year?: number,
+        public month?: number,
+        public day?: number,
+        public country?: string,
+        public state?: string,
+        public city?: string,
+        public names?: string[],
+        public keywords?: string[],
+        public occasion?: string,
+        public venue?: string,
+        public en?: string,
+        public da?: string,
+        public fileName?:string
+    ){}
+}
