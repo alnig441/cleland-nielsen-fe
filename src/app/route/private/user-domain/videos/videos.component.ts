d0edee1c94400e0a93013fc4adbd231b758760c3
Allan Nielsen <alnig441@gmail.com>
2019-07-21 11:02:47 -0400

    video component implemented

--- src/app/route/private/user-domain/videos/videos.component.ts
+++ src/app/route/private/user-domain/videos/videos.component.ts
@@ -1,23 +1,119 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { AuthenticationService } from "../../../../services/authentication.service";
 import { ServiceModelManagerService } from "../../../../services/service-model-manager.service";
+import { MongoVideoServices } from "../../../../services/mongoVideo.services";
+import { MongoVideoModel } from "../../../../models/mongoVideo.model";
 import { ActivatedRoute } from "@angular/router";
+import { SetMessageService } from "../../../../services/set-message.service";
 
 @Component({
     selector: 'app-events',
     template: require('./videos.component.pug'),
+    styleUrls: ['./videos.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 
 export class VideosComponent implements OnInit {
 
-    constructor(
-        private formManager: ServiceModelManagerService,
-        private activeUser: AuthenticationService,
-        private activatedRoute: ActivatedRoute
-    ){}
+  private albumViewSelector: object = new Object();
+  // private showVideoInformation: any[] = new Array();
+  private years: number[] = new Array();
+  private months: number[] = new Array();
+  private currentPage: number;
+  private doAnd: boolean = false;
+  private pages: number;
+  private total: number;
+  private documents: any[] = new Array();
+  private editVideos: any[] = new Array();
+  private videoModel = new MongoVideoModel();
+  // private videoList: string[] = new Array(6);
+  private selectAll: boolean = false;
+  // private modalSource: string;
 
-    ngOnInit(): void {
-        this.formManager.setService(this.activatedRoute.snapshot.url[0].path);
+  constructor(
+    private formManager: ServiceModelManagerService,
+    private activeUser: AuthenticationService,
+    private activatedRoute: ActivatedRoute,
+    private mongoVideoService: MongoVideoServices,
+    private message: SetMessageService
+  ){}
+
+  ngOnInit(): void {
+    this.years = [];
+    this.formManager.setService(this.activatedRoute.snapshot.url[0].path);
+    this.currentPage = 1;
+    this.buildAlbum();
+  }
+
+  buildAlbum() {
+    this.mongoVideoService.generateTabs()
+        .then((years: number[]) => {
+          if (years.length > 0) {
+            console.log('years: ', years);
+            this.years = years.reverse();
+            this.albumViewSelector['year'] = this.years[0];
+            this.mongoVideoService.generateTabs(this.years[0])
+                .then((months: number[]) => {
+                  console.log('months: ', months);
+                  this.months = months;
+                  this.albumViewSelector['month'] = this.months[this.months.length - 1];
+                  let model = new MongoVideoModel( null, this.years[0], this.months[this.months.length - 1] );
+                  this.mongoVideoService.search(model, this.currentPage, true)
+                      .then((result: any) => {
+                        this.pages = result.pages;
+                        this.total = result.total;
+                        this.documents = result.docs;
+                      })
+                })
+          }
+          else {
+            this.message.set({ status: 200, message: 'db currently empty'})
+          }
+        })
+  }
+
+  setAlbumView(selector?: any): void {
+    this.doAnd = true;
+    this.currentPage = 1;
+
+    if ( selector > 11) {
+      this.albumViewSelector['year'] = selector;
+      this.getTabs(selector, ( model: MongoVideoModel ) => {
+        this.getDocs( model, this.currentPage, this.doAnd );
+      })
     }
+
+    else {
+      this.albumViewSelector['month'] = selector;
+      let model = this.setModel(this.albumViewSelector['year'], selector);
+      this.getDocs( model, this.currentPage, this.doAnd );
+    }
+  }
+
+  getTabs ( selector: number, cb: any ) {
+    this.mongoVideoService.generateTabs( selector )
+      .then((months: any) => {
+        this.months = months;
+        this.albumViewSelector['month'] = this.months[this.months.length -1];
+        let model = this.setModel(this.albumViewSelector['year'], this.albumViewSelector['month']);
+        cb(model);
+      })
+  }
+
+  getDocs ( model: MongoVideoModel, page: number, doAnd: boolean ) {
+    // this.mongoImageService.search( model, page, doAnd )
+    //   .then((body: any) => {
+    //     this.pages = body.pages;
+    //     this.total = body.total;
+    //     this.documents = body.docs;
+    //   })
+  }
+
+  setModel ( year: number, month: number ) {
+    return new MongoVideoModel( null, year, month );
+  }
+
+  openEditor() : void {
+    this.editVideos = this.documents;
+  }
 }
