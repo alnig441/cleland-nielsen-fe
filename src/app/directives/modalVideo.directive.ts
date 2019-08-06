4403424061d4dff11a01bc7880c6996bd56f4bfc
Allan Nielsen <alnig441@gmail.com>
2019-08-06 10:38:42 -0400

    videoModal directive sizing according to viewPort, stylesheet/template adjusted accordingly

--- src/app/directives/modalVideo.directive.ts
+++ src/app/directives/modalVideo.directive.ts
@@ -1,5 +1,7 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from "@angular/core";
 
+const $ = require('jquery');
+
 @Directive({selector: '[modalVideo]'})
 
 export class ModalVideoDirective {
@@ -11,8 +13,13 @@ export class ModalVideoDirective {
 
     @Input() set modalVideo(condition: boolean) {
 
+
         if(condition){
+
             this.viewContainer.createEmbeddedView(this.templateRef);
+
+            let height = window.innerHeight * .8;
+            $('#video').attr('height', height);
 
             if(this.viewContainer.length > 1){
                 this.viewContainer.remove(0);
