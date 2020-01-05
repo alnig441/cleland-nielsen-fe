4949ddc8daf47df512a7d2b1f0fb69a386211bbf
Allan Nielsen <alnig441@gmail.com>
2020-01-05 18:40:01 -0500

    mongoImage model updated with getter/setter methods

--- src/app/models/mongoImage.model.ts
+++ src/app/models/mongoImage.model.ts
@@ -1,18 +1,58 @@
 export class MongoImageModel {
     constructor(
-        public created?: string,
-        public year?: number,
-        public month?: number,
-        public day?: number,
-        public country?: string,
-        public state?: string,
-        public city?: string,
-        public names?: string[],
-        public keywords?: string[],
-        public occasion?: string,
-        public venue?: string,
-        public en?: string,
-        public da?: string,
-        public fileName?:string
+        private created?: string,
+        private year?: number,
+        private month?: number,
+        private day?: number,
+        private country?: string,
+        private state?: string,
+        private city?: string,
+        private names?: string[],
+        private keywords?: string[],
+        private occasion?: string,
+        private venue?: string,
+        private en?: string,
+        private da?: string,
+        private fileName?:string
     ){}
+    
+    setYear(year: number) {
+      this.year = year;
+    }    
+    
+    setMonth(month: number) {
+      this.month = month;
+    }
+    
+    setDay(day: number) {
+      this.day = day;
+    }
+    
+    setNames(names: string[]) {
+      this.names = names;
+    }
+    
+    setKeywords(keywords: string[]) {
+      this.keywords = keywords;
+    }
+    
+    setOccasion(occasion: string) {
+      this.occasion = occasion;
+    }
+    
+    setVenue(venue: string) {
+      this.venue = venue;
+    }
+    
+    setCity(city: string) {
+      this.city = city;
+    }
+    
+    setState(state: string) {
+      this.state = state;
+    }
+    
+    setCountry(country: string) {
+      this.country = country;
+    }
 }
