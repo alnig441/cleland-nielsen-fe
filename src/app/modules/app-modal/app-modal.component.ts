2eda5c6a039ebf6515657039251b84a9b2775153
Allan Nielsen <alnig441@gmail.com>
2020-02-01 13:47:46 -0500

    ticker partial implementation

--- src/app/modules/app-modal/app-modal.component.ts
+++ src/app/modules/app-modal/app-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, DoCheck, AfterContentInit, AfterViewInit, ViewEncapsulation, HostListener } from "@angular/core";
+import { NgClass } from "@angular/common";
 import { AuthenticationServices } from "../../services/authentication.services";
 import { AppModalServices } from "../../services/app-modal.services";
 
@@ -15,29 +16,45 @@ export class AppModalComponent {
    keyword: any;
    interval: any;
    inc: number = 0;
+   keywords: any;
   
   constructor(
     private activeUser: AuthenticationServices,
     private modal:      AppModalServices,
   ) { 
-    this.modal.activeAsset.subscribe((asset: any) => {
+    this.modal.activeAsset.subscribe((asset: any) => {      
       let keywords = asset.meta.keywords;
+      this.keywords = asset.meta.keywords;
       this.keyword = keywords.length > 0 ? keywords[this.inc] : undefined ;
-      
-      if (this.keyword) {
-        
-        this.interval = setInterval(() => {
-          if(this.inc < asset.meta.keywords.length -1){
-            this.inc++;
-          }
-          else{
-            this.inc = Number(0);
-          }
-          this.keyword = asset.meta.keywords[this.inc];
-        }, 1500, asset)
-      }
-      
     })
+  }
+
+  @HostListener('transitionend', ['$event']) handler(event: TransitionEvent) {    
+    let id = $(event.target).attr('id');
+    let keywordsIsEmpty = this.keywords.length > 0 ? false : true;
+    
+    let type = !id ? 
+      'modal':
+      event.elapsedTime == 0.5 ?
+        'keyword_begin':
+        'keyword_end';
+    
+    if(!keywordsIsEmpty && type != 'keyword_begin'){
+      switch(type){
+        case 'modal':
+          $('#ticker p').addClass('begin');
+          break;
+        case 'keyword_end':
+          this.setKeyword();
+          break;
+      }
+    }
+    
+    if( type == 'keyword_begin' ) {
+      setTimeout(() => {
+        $(event.target).removeClass('begin');
+      }, 500)
+    }
   }
 
   @HostListener('document:keyup', ['$event']) keyupHandler( event: KeyboardEvent) {
@@ -53,9 +70,16 @@ export class AppModalComponent {
       clearInterval(this.interval)
     }
   }
+  
+  setKeyword() {
+    this.inc = (this.inc == this.keywords.length - 1) ? 0 : this.inc+=1 ;
+    this.keyword = this.keywords[this.inc];
+    $('#ticker p').addClass('begin');
+  }
 
   flip(direction: string): void {
     clearInterval(this.interval);
+    this.keyword = null;
     let assets = this.modal.getAssets();
     let source = this.modal.getSource();
     let currentImage = source.split('/')[2];
