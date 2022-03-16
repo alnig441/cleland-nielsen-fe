aad439b8104502724af50ab24a499cc355659ed4
alnig441 <alnig441@gmail.com>
2022-03-16 10:55:17 -0400

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./app/app.module";
-
-
 import "font-awesome/scss/font-awesome.scss";
 import "../config/font-awesome.config";
 
